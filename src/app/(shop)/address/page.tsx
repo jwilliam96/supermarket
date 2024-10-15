aabc90e80cbf1769771c5288aeaef1a78e297db6
jwilliam96 <jwsa.1996@gmail.com>
2024-10-14 23:02:27 -0500

    address

--- src/app/(shop)/address/page.tsx
+++ src/app/(shop)/address/page.tsx
@@ -2,7 +2,7 @@ import { Address } from "@/ui";
 
 export default function AddressPage() {
     return (
-        <div className="max-w-[1000px] mx-auto px-4 my-8">
+        <div className="max-w-[1000px] mx-auto px-4 mt-4 mb-10">
             <h2 className="text-3xl font-semibold">Dirección</h2>
             <span>Dirección de entrega</span>
             <Address />
--- src/ui/address/Address.tsx
+++ src/ui/address/Address.tsx
@@ -2,30 +2,31 @@ import { Button } from "@/components";
 
 export function Address() {
     return (
-        <form className="grid grid-cols-2 gap-10 my-12">
-
-
+        <form className="grid grid-cols-2 gap-10 mt-10 mb-20">
             <div className="space-y-3">
                 <div className="grid gap-2">
                     <label htmlFor="userName">Nombre</label>
-                    <input className="bg-gray-400" type="text" />
+                    <input className="px-6 bg-gray-300 py-2" type="text" />
                 </div>
                 <div className="grid gap-2">
                     <label htmlFor="address">Dirección</label>
-                    <input className="bg-gray-400" type="text" />
+                    <input className="px-6 bg-gray-300 py-2" type="text" />
                 </div>
 
                 <div className="grid gap-2">
                     <label htmlFor="postal">Código postal</label>
-                    <input className="bg-gray-400" type="text" />
+                    <input className="px-6 bg-gray-300 py-2" type="text" />
                 </div>
 
                 <div className="grid gap-2">
                     <label htmlFor="country">País</label>
-                    <input className="bg-gray-400" type="text" />
+                    <input className="px-6 bg-gray-300 py-2" type="text" />
                 </div>
 
-                <label htmlFor="checkbox" className="block mt-10"><input className="bg-gray-400" type="checkbox" />¿Recordar dirección? </label>
+                <div className="py-4 flex gap-4 items-center">
+                    <div className="size-5 border border-gray-500 rounded-md"></div>
+                    <span>¿Recordar dirección?</span>
+                </div>
 
                 <Button text="Siguiente" className="w-full rounded-none" />
 
@@ -34,30 +35,26 @@ export function Address() {
             <div className="space-y-3">
                 <div className="grid gap-2">
                     <label htmlFor="lastName">Apellido</label>
-                    <input className="bg-gray-400" type="text" />
+                    <input className="px-6 bg-gray-300 py-2" type="text" />
                 </div>
 
 
                 <div className="grid gap-2">
                     <label htmlFor="addressOptional">Dirección 2 (opcional)</label>
-                    <input className="bg-gray-400" type="text" />
+                    <input className="px-6 bg-gray-300 py-2" type="text" />
                 </div>
 
                 <div className="grid gap-2">
                     <label htmlFor="city">Ciudad</label>
-                    <input className="bg-gray-400" type="text" />
+                    <input className="px-6 bg-gray-300 py-2" type="text" />
                 </div>
 
 
                 <div className="grid gap-2">
                     <label htmlFor="phone">Teléfono</label>
-                    <input className="bg-gray-400" type="text" />
+                    <input className="px-6 bg-gray-300 py-2" type="text" />
                 </div>
-
             </div>
-
-
-
         </form>
     )
 }
