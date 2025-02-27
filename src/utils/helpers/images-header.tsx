51c30b318d8772ed78c65bac0447e4217b416cd7
jwilliam96 <jwsa.1996@gmail.com>
2025-02-27 15:03:12 -0500

    se agregan imagenes mas optimizadas al header

--- src/utils/helpers/images-header.tsx
+++ src/utils/helpers/images-header.tsx
@@ -1,2 +1,2 @@
 
-export const imagesHeader = [{ img: "/home/img1.png", description: "pan" }, { img: "/home/img2.png", description: "cafe" }, { img: "/home/img3.png", description: "oso de peluche" }]
+export const imagesHeader = [{ img: "/home/header/pan.webp", description: "pan" }, { img: "/home/header/cafe.webp", description: "cafe" }, { img: "/home/header/pañales.webp", description: "oso de peluche" }]
